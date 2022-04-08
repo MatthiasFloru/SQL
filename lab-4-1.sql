9dac74de4a96c41137237916cf1763a2c20dffd8
MatthiasFloru <matthias.floru@kellogg.northwestern.edu>
2022-04-08 21:17:02 +0000

    updated with further lab exercises during class

--- lab-4-1.sql
+++ lab-4-1.sql
@@ -3,4 +3,13 @@
 -- Expected result:
 -- 2935
 
+-- The first one is wrong because it takes an ID from stats which is not the player ID
+SELECT players.first_name, players.last_name, stats.hits, sum(hits)
+FROM players INNER JOIN stats ON stats.id = players.id
+WHERE players.first_name = "Barry" AND players.last_name = "Bonds"
+;
 
+-- This one is correct
+SELECT sum(hits) FROM stats INNER JOIN players ON stats.player_id = players.id
+WHERE players.first_name = "Barry" AND players.last_name = "Bonds"
+;
