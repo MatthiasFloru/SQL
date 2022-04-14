57b724658b454632eb8ffd446eb6ae0905571712
MatthiasFloru <matthias.floru@kellogg.northwestern.edu>
2022-04-14 14:36:55 +0000

    finished the lab outside of class

--- lab-4-2.sql
+++ lab-4-2.sql
@@ -24,4 +24,10 @@
 -- | Billy      | Hamilton  |
 -- | Ian        | Happ      |
 
-
+SELECT players.first_name, players.last_name
+FROM stats 
+INNER JOIN players ON players.id = stats.player_id 
+INNER JOIN teams ON stats.team_id = teams.id
+WHERE teams.name = "Chicago Cubs"
+AND teams.year = 2020
+;
